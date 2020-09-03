a86eea828326f2a0c978b200cd0d91bd2759d0ac
William Hudson <williamdhudson@gmail.com>
2020-09-02 20:42:56 -0400

    MongoDb seeded through mongoimport

--- database-mongo/seed.js
+++ database-mongo/seed.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://images', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/images', { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 var db = mongoose.connection;
@@ -10,12 +10,45 @@ db.on('error', function (err) {
 
 db.once('open', function () {
   console.log('mongoose connected successfully');
-});
+})
+  .then(() => { seedMongo() })
+  .catch((err) => console.log('error with seeding mongo database: ', err))
 
-var mediaSchema = mongoose.Schema({
-  id: { type: Number, unique: true },
+
+
+var imageSchema = mongoose.Schema({
+  itemId: { type: Number, unique: true },
   pictureId1: Number,
   pictureId2: Number
 })
 
-var Item = mongoose.model('Item', mediaSchema);
+var Image = mongoose.model('Image', imageSchema);
+
+let seedMongo = () => {
+  console.time('Time to seed Mongo Database: ')
+  let counter = 99
+  let totalEntries = 10000100
+  let pictureId = 1
+  while (counter < totalEntries) {
+    counter++
+    var newImage = new Image({
+      itemId: counter,
+      pictureId1: pictureId,
+      pictureId2: pictureId + 1
+    })
+    newImage.save()
+      .then(() => {
+        if (i === totalEntries - 1) {
+          console.timeEnd('Time to seed Mongo Database: ')
+          db.close()
+        }
+      })
+      .catch(err => console.log('error with newImage.save: ', err))
+    pictureId += 2
+    if (pictureId > 2000) {
+      pictureId = 1
+    }
+  }
+}
+
+// db.dropDatabase()
