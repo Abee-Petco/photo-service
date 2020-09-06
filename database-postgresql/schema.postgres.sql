86875ed217fb3a026a6635c17b6cc9f6bfaa6a21
William Hudson <williamdhudson@gmail.com>
2020-09-06 15:59:30 -0400

    Seed both databases correctly

--- database-postgresql/schema.postgres.sql
+++ database-postgresql/schema.postgres.sql
@@ -1,13 +1,13 @@
 CREATE TABLE images (
   itemId INTEGER PRIMARY KEY,
-  pictureId1 INTEGER NOT NULL,
-  pictureId2 INTEGER NOT NULL,
-  smallPic BOOLEAN,
-  medPic BOOLEAN,
-  largePic BOOLEAN
+  pictureId1 VARCHAR (50) NOT NULL,
+  pictureId2 VARCHAR (50) NOT NULL,
+  smallPic BOOLEAN NOT NULL,
+  medPic BOOLEAN NOT NULL,
+  largePic BOOLEAN NOT NULL
  );
 
- CREATE TABLE sizes (
+ CREATE TABLE sizeurls (
    id INTEGER PRIMARY KEY,
    size VARCHAR(50),
    size_url VARCHAR(50)
